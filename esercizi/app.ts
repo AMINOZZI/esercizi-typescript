91a86b8800e77d731027a12448ab53cf60e13a70
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-06 17:08:02 +0200

    use type in typescript function

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+console.log("Your code goes here...");
+// add a function with 2 argument number and return sun
+function add(n1, n2) {
+  return n1 + n2;
+}
+//create 2 const number
+const number1 = 10;
+const number2 = 5;
+//use add method and create a const result
+const result = add(number1, number2);
+
+//result
+console.log(result);
