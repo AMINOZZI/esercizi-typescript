b1215d3af086cdf4193ae8995d0637e9466e85f3
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-12 10:40:39 +0200

    nested object

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -7,3 +7,14 @@ const person = {
   age: 33,
 };
 console.log(person.name);
+
+//nested
+const product = {
+  id: "abc1",
+  price: 12.99,
+  tags: ["great-offer", "hot-and-new"],
+  details: {
+    title: "Red Carpet",
+    description: "A great carpet - almost brand-new!",
+  },
+};
