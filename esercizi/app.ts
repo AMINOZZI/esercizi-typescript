924ace904b5bc0f07f8929c95a39bcb3e3fa4808
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-12 09:39:20 +0200

    add a phrase

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -2,7 +2,7 @@ console.log("Your code goes here...");
 // add a function with 4 argument with condition of showResult and return sun
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(phrase + n1 + n2);
   } else {
     return n1 + n2;
   }
