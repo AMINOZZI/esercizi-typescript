2d21ac74cce0c7e1fe96de10d5166966273fbad9
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-12 09:37:17 +0200

    insert boolean  and if condition

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -1,15 +1,19 @@
 console.log("Your code goes here...");
-// add a function with 2 argument number and return sun
-function add(n1: number, n2: number) {
-  //check typeof
-  console.log(typeof number1);
-  return n1 + n2;
+// add a function with 4 argument with condition of showResul   and return sun
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
-//create 2 const number
+//create 4 const number
 const number1 = 10;
 const number2 = 5;
+const printResult = true;
+const resultPhrase = "Result is :";
 //use add method and create a const result
-const result = add(number1, number2);
+const result = add(number1, number2, printResult, resultPhrase);
 
 //result
 console.log(result);
