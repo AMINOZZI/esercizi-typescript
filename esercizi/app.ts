70a05af670f4764c2117edf01105788b8aa0b10e
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-12 10:38:50 +0200

    create object person

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -1,20 +1,9 @@
-console.log("Your code goes here...");
-// add a function with 4 argument with condition of showResult and return sun
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
-//create 4 const number
-const number1 = 10;
-const number2 = 5;
-const printResult = true;
-const resultPhrase = "Result is :";
-//use add method and create a const result
-const result = add(number1, number2, printResult, resultPhrase);
-
-//result
-console.log(result);
+//create object
+const person = {
+  //   name: string;
+  //   age: number;
+  // } = {
+  name: "Amedeo",
+  age: 33,
+};
+console.log(person.name);
