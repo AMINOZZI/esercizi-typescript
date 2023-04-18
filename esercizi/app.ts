c7809214d2f779079c8b4530c821378356944911
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-18 17:52:14 +0200

    block scope and use typeof

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -1,7 +1,10 @@
-function combine(input1: number, input2: number) {
-  const result = input1 + input2;
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  }
   return result;
 }
-//combine function two number 
+//combine function two number
 const conbineAges = combine(30, 34);
 console.log(conbineAges);
