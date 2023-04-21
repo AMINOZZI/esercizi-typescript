140b406ac700b7b733c114d2c4e4e5c26e82f012
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-21 15:22:36 +0200

    combine number and string with if check

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -3,8 +3,11 @@ function combine(input1: number | string, input2: number | string) {
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   }
-  return result;
+  return (result = input1.toString() + input2.toString());
 }
 //combine function two number
 const conbineAges = combine(30, 34);
+console.log(conbineAges);
+//combine function two string
+const combineName = combine("jenny", "mary");
 console.log(conbineAges);
