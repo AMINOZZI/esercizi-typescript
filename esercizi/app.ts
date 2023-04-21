5784730904d0530a84b9198eff60bcec09959c37
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-21 15:25:08 +0200

    fix

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -2,12 +2,14 @@ function combine(input1: number | string, input2: number | string) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
   }
-  return (result = input1.toString() + input2.toString());
+  return result;
 }
 //combine function two number
 const conbineAges = combine(30, 34);
 console.log(conbineAges);
 //combine function two string
 const combineName = combine("jenny", "mary");
-console.log(conbineAges);
+console.log(combineName);
