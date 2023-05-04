598dc9a38f61f9e50e0fb5256e787dfc1446089a
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-05-04 15:00:02 +0200

    the never type

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -3,8 +3,13 @@ let userName: string;
 
 userInput = 5;
 userInput = "AME";
-//se definisco una variabile con unKnown il tipo deve passare dentro un if 
+//se definisco una variabile con unKnown il tipo deve passare dentro un if
 //per evitare che ci siano problemi di assegnazione
 if (typeof userInput === "string") {
   userName = userInput;
 }
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError("an error occurred!", 500);
