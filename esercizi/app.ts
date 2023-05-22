1d2facfa7e414d99c96849cee2c00d4ae8f78296
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-05-22 11:03:01 +0200

    interface

--- esercizi/app.ts
+++ esercizi/app.ts
@@ -1,106 +1,18 @@
-class Department {
-  // private readonly id: string;
-  // private name: string;
-  //accesso dalla sotto classe
-  protected employees: string[] = [];
-
-  constructor(private readonly id: string, public name: string) {
-    // this.id = id;
-    // this.name = n;
-  }
-
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
-  }
-
-  addEmployee(employee: string) {
-    // validation etc
-    // this.id = 'd2';
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
-}
-//esempio di sub class estensione della classe padre :si può estendere da una sola classe
-class ITDepartment extends Department {
-  admins: string[];
-  constructor(id: string, admins: string[]) {
-    super(id, "IT");
-    this.admins = admins;
-  }
+//l'intefaccia dichiara solo le proprietà nel suo oggetto..non può essere utilizzata come factory o singleton
+//Le interfacce non posso usarle per creare oggetti. 
+//Sono classi astratte con metodi senza corpo. Inoltre non possono contenere costruttori.
+interface Pizza {
+  name: string;
+  toppings: string[];
 }
 
-class DesignDepartment extends Department {
-  admins: string[];
-  constructor(id: string, admins: string[]) {
-    super(id, "Design");
-    this.admins = admins;
+class PizzaMaker {
+  static create(event: Pizza) {
+    return { name: event.name, toppings: event.toppings };
   }
 }
-
-class AccountingDepartment extends Department {
-  private lastReport: string;
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No report found");
-  }
-
-  set mostRecentReport(value) {
-    if (!value) {
-      throw new Error("No value found");
-    }
-    this.addReport(value);
-  }
-
-  constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    this.lastReport = reports[0];
-  }
-  addEmployee(name: string) {
-    if (name === "Max") {
-      return;
-    }
-    this.employees.push(name);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-const it = new ITDepartment("d1", ["Max"]);
-
-const des = new DesignDepartment("d3", ["Amedeo"]);
-
-it.addEmployee("Max");
-it.addEmployee("Manu");
-
-// it.employees[2] = 'Anna';
-
-it.describe();
-it.name = "NEW NAME";
-it.printEmployeeInformation();
-
-console.log(it);
-
-const accounting = new AccountingDepartment("d2", []);
-accounting.mostRecentReport = "Set value";
-accounting.addReport("Something went wrong...");
-console.log(accounting.mostRecentReport, "qui");
-
-accounting.printReports();
-
-// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-
-// accountingCopy.describe();
+//const instanza = new Pizza()// non possiamo instanziare un interfaccia
+const pizza = PizzaMaker.create({
+  name: "Inferno",
+  toppings: ["cheese", "peppers"],
+});
---
+++ esercizi/class-esempio.ts
@@ -0,0 +1,106 @@
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  //accesso dalla sotto classe
+  protected employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    // this.id = 'd2';
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+//esempio di sub class estensione della classe padre :si può estendere da una sola classe
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class DesignDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "Design");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value) {
+    if (!value) {
+      throw new Error("No value found");
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Max"]);
+
+const des = new DesignDepartment("d3", ["Amedeo"]);
+
+it.addEmployee("Max");
+it.addEmployee("Manu");
+
+// it.employees[2] = 'Anna';
+
+it.describe();
+it.name = "NEW NAME";
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment("d2", []);
+accounting.mostRecentReport = "Set value";
+accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport, "qui");
+
+accounting.printReports();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
